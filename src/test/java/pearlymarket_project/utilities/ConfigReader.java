515cccec424115fc31eed9486e7edc7437348320
fierro <gasimovf98@gmail.com>
2023-05-31 01:10:15 -0400

    pushing project framework

---
+++ src/test/java/pearlymarket_project/utilities/ConfigReader.java
@@ -0,0 +1,41 @@
+package pearlymarket_project.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+
+    //This class will read configuration.properties file using JAVA
+
+    //Create Properties object
+    private static Properties properties;
+
+    //static block it will read the file before everything
+    static {
+        String path = "configuration.properties";
+        try {
+            FileInputStream fileInputStream = new FileInputStream(path);
+            properties = new Properties();
+            properties.load(fileInputStream);
+            fileInputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Create method that accepts KEY and returns VALUE
+    public static String getProperty(String key){
+
+        String value = properties.getProperty(key);
+        return value;
+    }
+
+    //TEST
+    public static void main(String[] args) {
+        System.out.println(ConfigReader.getProperty("amazon_prod_url"));
+        System.out.println(ConfigReader.getProperty("amazon_qa_url"));
+        System.out.println(ConfigReader.getProperty("browser"));
+    }
+}
---
+++ src/test/java/pearlymarket_project/utilities/JSUtils.java
@@ -0,0 +1,67 @@
+package pearlymarket_project.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.time.Duration;
+
+public class JSUtils {
+
+
+    private static WebDriver driver;
+
+    //1. getDriver : setup instance the driver object
+    public static WebDriver getDriver(){
+        if (driver==null){
+
+            switch (ConfigReader.getProperty("browser")){
+                case "chrome" :
+                    WebDriverManager.chromedriver().setup();
+                    driver=new ChromeDriver();
+                    break;
+
+                case "firefox" :
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+
+                case "chrome-headless" :
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    break;
+
+                case "edge" :
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+
+                case "safari" :
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                    break;
+
+            }
+
+
+
+        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    //2. closeDriver : quits the driver
+    public static void closeDriver(){
+
+        if (driver!=null){
+            driver.quit();
+            driver=null;
+        }
+
+    }
+}
