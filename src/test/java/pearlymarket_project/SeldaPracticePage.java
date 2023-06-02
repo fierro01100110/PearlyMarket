4c2b2898c27ed417952310aaef50a09dc255ae64
selda75 <127150726+selda75@users.noreply.github.com>
2023-06-02 12:51:27 -0600

    new meaasage

--- src/test/java/pearlymarket_project/SeldaPracticePage.java
+++ src/test/java/pearlymarket_project/SeldaPracticePage.java
@@ -1,7 +1,11 @@
 package pearlymarket_project;
 
 public class SeldaPracticePage {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello everyone");
+
+
+        System.out.println("Hello to everyone");
     }
 }
