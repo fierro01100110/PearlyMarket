79f79b551676dd78ee8c6d4da44da68998fbbf5a
Seyma Nursah Akcin <seymanursah00@gmail.com>
2023-06-05 12:07:56 -0400

    seyma

---
+++ src/test/java/pearlymarket_project/pages/PearlyMarketSignInPage.java
@@ -0,0 +1,24 @@
+package pearlymarket_project.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import pearlymarket_project.utilities.Driver;
+
+public class PearlyMarketSignInPage {
+
+    public PearlyMarketSignInPage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id = "username")
+    public WebElement usernameOrEmail;
+
+    @FindBy(id = "password")
+    public WebElement password;
+
+    @FindBy(xpath = "//button[text()='Sign In']")
+    public WebElement signIn;
+
+
+}
