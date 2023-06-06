9d643fa5aa64b40e11b1ae9096401440b62dc161
Feyza Mollaoglu <mollaoglufeyza@gmail.com>
2023-06-06 01:18:55 -0400

    feyza

--- src/test/java/pearlymarket_project/FeyzaM.java
+++ src/test/java/pearlymarket_project/FeyzaM.java
@@ -2,6 +2,7 @@ package pearlymarket_project;
 
 public class FeyzaM {
     public static void main(String[] args) {
+
         System.out.println("hi");
     }
 }
--- src/test/java/pearlymarket_project/SeldaPracticePage.java
+++ src/test/java/pearlymarket_project/SeldaPracticePage.java
@@ -4,6 +4,7 @@ public class SeldaPracticePage {
 
 
     public static void main(String[] args) {
+
         System.out.println("Hello to everyone");
     }
 }
