9f1f7ba18cb1b9de2a6b1c0a2885eb8f3c90f0a6
Seyma Nursah Akcin <seymanursah00@gmail.com>
2023-06-10 15:23:16 -0400

    seyma1

--- src/test/java/pearlymarket_project/utilities/ExtentReportUtils.java
+++ src/test/java/pearlymarket_project/utilities/ExtentReportUtils.java
@@ -14,26 +14,6 @@ public class ExtentReportUtils {
     protected static ExtentHtmlReporter extentHtmlReporter;
     protected static ExtentTest extentTest;
 
-
-
-    /*
-    We use static block here. Whenever I call ExtentReportUtils class, static block will run and do the necessary configurations
-    before using variables.
-     */
-
-    static{
-
-
-        //        WHAT WILL BE REPORT NAME AND WHERE THE REPORT IS CREATED
-//        PATH
-        String now = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
-        String path = System.getProperty("user.dir")+"/test-output/Reports/"+now+"extent_reports.html";
-//        Create the HTML template using extent html reporter in the path
-        extentHtmlReporter = new ExtentHtmlReporter(path);
-//        Create extent report
-        extentReports = new ExtentReports();
-//        ***********ADDING CUSTOM SYSTEM INFORMATION ***********
-
     /*
     We use static block here. Whenever I call ExtentReportUtils class static block will run
     and do necessary configurations before using variables.
@@ -52,7 +32,6 @@ public class ExtentReportUtils {
         extentReports = new ExtentReports();
 
 //      ***********ADDING CUSTOM SYSTEM INFORMATION ***********
-
         extentReports.setSystemInfo("Application Name","Apple M1 Unit");
         extentReports.setSystemInfo("Test Environment","Regression");
         extentReports.setSystemInfo("Browser","Chrome");
@@ -69,40 +48,6 @@ public class ExtentReportUtils {
         extentReports.attachReporter(extentHtmlReporter);
 //        **********CREATE EXTENT TEST THAT IS ALSO KNOWN AS LOGGER**
         extentTest=extentReports.createTest("MY REGRESSION","MY FIRST EXTENT REPORT");
-
-    }
-
-
-    //LOGGER METHODS
-
-    //   ExtentReportUtils.pass("Message") --> Logs the message as passed on report
-    public static void pass(String message) {
-        extentTest.pass(message);
-    }
-
-    //   ExtentReportUtils.info("Message") --> Logs the message as information on report
-    public static void info(String message) {
-        extentTest.info(message);
-    }
-
-    //   ExtentReportUtils.fail("Message") --> Logs the message as failure on report
-    public static void fail(String message) {
-        extentTest.fail(message);
-    }
-
-    //This method will log the pass message on report and attached the screenshot of that step on report.
-    public static void passAndCaptureScreenshot(String message) throws IOException {
-        extentTest.pass(message).addScreenCaptureFromPath(MediaUtils.takeScreenshotOfTheEntirePageAsString());
-    }
-
-
-    //To generate the extent report we must use "extentReports.flush();"
-    //ExtentReportUtils.flush(); --> Generates report
-    public static void flush(){
-        extentReports.flush();
-    }
-
-
 
     }
 
@@ -133,5 +78,4 @@ public class ExtentReportUtils {
     public static void flush(){
         extentReports.flush();
     }
-
 }
